b19fe602ae9ee6cc7b00400bf54d735f9ed03095
Kevin Old <kevin@kevinold.com>
2015-12-16 22:08:50 -0600

    upgrade devtools; add redux routing structure

--- js/containers/App.js
+++ js/containers/App.js
@@ -2,24 +2,23 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import configureStore from '../store/configureStore';
 import Home from '../components/Home';
-import {renderDevTools} from '../utils/devTools';
+import { Redirect, Route } from 'react-router';
+import { ReduxRouter } from 'redux-router';
+import DevTools from './DevTools';
 
 const store = configureStore();
 
 export default React.createClass({
   render() {
     return (
-      <div>
-
-        {/* <Home /> is your app entry point */}
-        <Provider store={store}>
-          <Home />
-        </Provider>
-
-        {/* only renders when running in DEV mode */
-          renderDevTools(store)
-        }
-      </div>
+      <Provider store={store}>
+        <div>
+          <ReduxRouter>
+            <Route path="/" component={Home} />
+          </ReduxRouter>
+          <DevTools />
+        </div>
+      </Provider>
     );
   }
 });
