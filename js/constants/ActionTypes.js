93ff79b91a734270923588e6dfe48915cda26f5b
davidwells <david@inboundnow.com>
2016-11-07 19:05:52 -0800

    remove old files

--- js/constants/ActionTypes.js
+++
@@ -1,4 +0,0 @@
-export const DATA_FETCHED = 'DATA_FETCHED';
-export const DATA_SUCCEEDED = 'DATA_SUCCEEDED';
-export const DATA_FAILED = 'DATA_FAILED';
-export const TITLE_CHANGED = 'TITLE_CHANGED';
--- js/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { routeReducer } from 'redux-simple-router';
-import app from './app';
-
-export default combineReducers({
-  app: app,
-  routing: routeReducer
-});
