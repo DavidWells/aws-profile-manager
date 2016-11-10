a54a641a13bbd2719c81edb6d46a5d8cc6b58647
Nik Graf <nik@nikgraf.com>
2016-11-10 11:56:05 +0100

    track user id

--- app/index.js
+++ app/index.js
@@ -24,19 +24,26 @@ webFrame.setZoomLevelLimits(1, 1)
 
 /* IPC for debuging */
 ipcRenderer.send('ui-ready', 'ping')
-ipcRenderer.on('debugFromMain', (e, arg) => {
-  console.log(arg)
+ipcRenderer.on('userId', (evt, userId) => {
+  // eslint-disable-next-line no-console
+  console.log(`%cUserId: ${userId}`, 'color: blue')
+  if (process.env.NODE_ENV !== 'development') {
+    window.analytics.identify(userId)
+  }
 })
 
 checkForUpdates().then((data) => {
   if (data.version) {
+    // eslint-disable-next-line no-console
     console.log('version out of date!')
+    // eslint-disable-next-line no-console
     console.log('new version data', data)
     // do update alert
   }
 })
 
 const specificDataByID = getAWSProfileData('serverless_dev')
+// eslint-disable-next-line no-console
 console.log('specificDataByID', specificDataByID)
 // const awsCredsFileAfterDelete = deleteAWSProfile('serverless_staging')
 // console.log('awsCredsFileAfterDelete', awsCredsFileAfterDelete)
