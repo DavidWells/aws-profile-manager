9186657fe9421c640bdaf42cb1281ecac44f9ce1
davidwells <david@inboundnow.com>
2016-11-09 22:44:12 -0800

    update atomicWrite file

--- app/utils/electron/atomicWriteFile.js
+++ app/utils/electron/atomicWriteFile.js
@@ -1,10 +1,30 @@
 import fs from 'fs'
 
-// eslint-disable-next-line import/prefer-default-export
-export function atomicWriteFileSync(filepath, content) {
-  // TODO: add random number generator
-  const randomId = 'lsdalalkjdlkjdlksjdlkjdalk'
+export default function atomicWriteFileSync(filepath, content, callback) {
+  const randomId = backupID()
   const backupPath = `${filepath}.${randomId}.bak`
-  fs.writeFileSync(backupPath, content)
-  fs.renameSync(backupPath, filepath)
+  try {
+    fs.writeFileSync(backupPath, content)
+  } catch (e) {
+    throw e
+  }
+  try {
+    // TODO: backup silently fails. Might need to backup to a different spot
+    console.log('do rename')
+    console.log('backupPath', backupPath)
+    console.log('filepath', filepath)
+    fs.renameSync(backupPath, filepath)
+  } catch (e) {
+    throw e
+  }
+  if (callback) {
+    callback()
+  }
+}
+
+const rand = () => {
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+}
+const backupID = () => {
+  return `local-${rand()}${rand()}-${rand()}`
 }
