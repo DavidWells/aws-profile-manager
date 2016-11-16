ac48baa9074abf5ee96ade8b5e723364f986785a
davidwells <david@inboundnow.com>
2016-11-15 22:42:18 -0800

    add update check

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,7 +1,9 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
+import { shell } from 'electron'
 import Logo from '../../static/images/bolt.svg'
 import Feedback from '../components/Feedback'
+import checkForUpdates from '../utils/checkForUpdates'
 import SettingsIcon from '../../static/images/settings.svg'
 import styles from './App.css'
 
@@ -10,8 +12,41 @@ export default class App extends Component {
   static propTypes = {
     children: PropTypes.element.isRequired
   };
-
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      update: false
+    }
+  }
+  componentDidMount() {
+    checkForUpdates().then((data) => {
+      console.log('check', data)
+      if (data.version) {
+        this.setState({
+          update: data
+        })
+        // eslint-disable-next-line no-console
+        console.log('version out of date!')
+        // eslint-disable-next-line no-console
+        console.log('new version data', data)
+        // do update alert
+      }
+    })
+  }
+  handleDownload = (e) => {
+    e.preventDefault()
+    shell.openExternal('http://bit.ly/serverless-dashboard')
+  }
   render() {
+    const { update } = this.state
+    let updateText
+    if (update) {
+      updateText = (
+        <div className={styles.update} onClick={this.handleDownload}>
+          🎉 Good news. An update is available! <b>Click to Download v{update.version} 🚀</b>
+        </div>
+      )
+    }
     return (
       <div className={styles.app}>
         <div className={styles.navigationWrapper}>
@@ -26,6 +61,7 @@ export default class App extends Component {
             </div>
             <div className={styles.middleNav}>
               <Link to='/'>Services</Link>
+              {updateText}
             </div>
             <div className={styles.rightNav}>
               <Link className={styles.settings} to='/settings'>
