0a209828ff83c42100828aaecd309d394c1b2453
davidwells <david@inboundnow.com>
2016-11-16 01:26:40 -0800

    clean up index

--- app/index.js
+++ app/index.js
@@ -6,47 +6,36 @@ import { Router, hashHistory } from 'react-router'
 import routes from './routes'
 import configureStore from './store/configureStore'
 import loadServicesFromStorage from './utils/loadServicesFromStorage'
-import { getAWSCredentials, getAWSProfileData } from './utils/aws'
-import checkForUpdates from './utils/checkForUpdates'
+import newHandleUser, { setUUID } from './utils/serverless/newUser'
+import { getAWSCredentials } from './utils/aws'
 import setupErrorTracking from '../desktop/utils/setupErrorTracking'
 import './app.global.css'
 
 if (process.env.NODE_ENV !== 'development') {
   setupErrorTracking()
 }
-
+setUUID()
 // Disable pinch zoom
 webFrame.setZoomLevelLimits(1, 1)
 
 /* IPC for debuging */
 ipcRenderer.send('ui-ready', 'ping')
 ipcRenderer.on('userId', (evt, userId) => {
+  if (!userId) {
+    newHandleUser()
+  }
   // eslint-disable-next-line no-console
   // console.log(`%cUserId: ${userId}`, 'color: blue')
   if (process.env.NODE_ENV !== 'development') {
-    window.analytics.identify(userId)
+    if (userId) {
+      window.analytics.identify(userId)
+    }
   }
 })
 
-checkForUpdates().then((data) => {
-  if (data.version) {
-    // eslint-disable-next-line no-console
-    console.log('version out of date!')
-    // eslint-disable-next-line no-console
-    console.log('new version data', data)
-    // do update alert
-  }
-})
-
-const specificDataByID = getAWSProfileData('serverless_dev')
-// eslint-disable-next-line no-console
-console.log('specificDataByID', specificDataByID)
-// const awsCredsFileAfterDelete = deleteAWSProfile('serverless_staging')
-// console.log('awsCredsFileAfterDelete', awsCredsFileAfterDelete)
-
-const safedServices = loadServicesFromStorage()
+const savedServices = loadServicesFromStorage()
 const initialState = {
-  services: safedServices,
+  services: savedServices,
   credentials: getAWSCredentials()
 }
 const store = configureStore(initialState)
